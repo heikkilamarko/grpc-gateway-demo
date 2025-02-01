87d6dc307887d9a91a98708999e40bc7e5f52e90
Marko Heikkilä <heikkila_marko@hotmail.com>
2025-02-01 11:31:58 +0200

    Add scheduled http request pipeline.

--- infra/postgres/init.sql
+++ infra/postgres/init.sql
@@ -1,1 +1,20 @@
 CREATE DATABASE grafana;
+
+CREATE DATABASE grpc_gateway_demo;
+
+\c grpc_gateway_demo;
+
+CREATE SCHEMA demo;
+
+CREATE TABLE demo.success_requests (
+    id SERIAL PRIMARY KEY,
+     data JSONB NOT NULL,
+     created_at timestamptz NOT NULL DEFAULT now()
+);
+
+CREATE TABLE demo.error_requests (
+    id SERIAL PRIMARY KEY,
+    error_message TEXT,
+    data JSONB NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT now()
+);
